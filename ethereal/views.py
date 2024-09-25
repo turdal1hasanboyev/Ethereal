f66da34a589ca1b8b44cdd76b77755dfa111aede
turdal1hasanboyev <turdalihasanboyev70.com>
2024-09-25 17:32:29 +0500

    Written backend!

--- ethereal/views.py
+++ ethereal/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
-
+from .models import Testimonial, Contact, Ethereal, About, Work
 
 
 def home(request):
-    return render(request, 'index.html')
+    ethereal = Ethereal.objects.all().order_by('id')[:3]
+    work = Work.objects.all().order_by('-id')[4:]
+    testimonial = Testimonial.objects.all().order_by('id')[:3]
+    about = About.objects.get(id=1)
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        Contact.objects.create(
+            name=name,
+            email=email,
+            message=message,
+        )
+
+        return redirect('home')
+    
+    context = {
+        'ethereal': ethereal,
+        'work': work,
+        'testimonial': testimonial,
+        'about': about,
+    }
+
+    return render(request, 'index.html', context)
