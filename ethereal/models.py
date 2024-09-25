556525238978d83c3dff9446a0ddc381f1c2d76f
turdal1hasanboyev <turdalihasanboyev70.com>
2024-09-25 16:28:48 +0500

    Default migrate and written account model!

--- ethereal/models.py
+++ ethereal/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class Account(AbstractUser):
+    image = models.ImageField(upload_to='accounts_images/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
